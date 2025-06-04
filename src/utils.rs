3d636e484e622cb2e2c68cc4e10c41a705f3e0a7
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-04 06:29:43 -0400

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -3,13 +3,13 @@ use futures_util::stream::StreamExt;
 use std::fs;
 use std::io::Write;
 
-pub async fn save_file(mut payload: Multipart, path: &str) -> Result<String, std::io::Error> {
+pub async fn save_file(mut payload: Multipart) -> Result<String, std::io::Error> {
     let mut file_path = String::new();
     while let Some(item) = payload.next().await {
         let mut field = item?;
         let content_disposition = field.content_disposition().unwrap();
         let filename = content_disposition.get_filename().unwrap_or("unknown");
-        let filepath = format!("{}/{}", path, filename);
+        let filepath = format!("/data/uploads/{}", filename);
         let mut f = fs::File::create(&filepath)?;
         while let Some(chunk) = field.next().await {
             let data = chunk?;
