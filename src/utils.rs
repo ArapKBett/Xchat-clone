cc16347dca6269d8b7f320c985e876b2e1399f59
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-04 08:04:34 -0400

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use actix_multipart::{Multipart, MultipartError};
+use actix_multipart::Multipart;
 use futures_util::StreamExt;
 use std::fs;
 use std::io::{self, Write};
@@ -7,16 +7,16 @@ use bytes::Bytes;
 pub async fn save_file(mut payload: Multipart) -> Result<String, io::Error> {
     let mut file_path = String::new();
     while let Some(item) = payload.next().await {
-        let field = item.map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-        let content_disposition = field.content_disposition();
+        let field = item.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+        let content_disposition = field.content();
         let filename = content_disposition
-            .and_then(|cd| cd.get_filename().map(str::to_string))
-            .unwrap_or_else(|| "unknown".to_string());
+            .get_filename()
+            .map_or_else(|| "unknown".to_string(), |name| name.to_string());
         let filepath = format!("/data/uploads/{}", filename);
         let mut f = fs::File::create(&filepath)?;
         let mut field = field;
         while let Some(chunk) = field.next().await {
-            let data: Bytes = chunk.map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+            let data: Bytes = chunk.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
             f.write_all(&data)?;
         }
         file_path = filepath;
