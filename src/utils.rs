62946ff89ca6b0e78391edaf42d5da67bf4cdf1e
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-04 08:16:32 -0400

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -8,10 +8,11 @@ pub async fn save_file(mut payload: Multipart) -> Result<String, io::Error> {
     let mut file_path = String::new();
     while let Some(item) = payload.next().await {
         let field = item.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
-        let content_disposition = field.content();
-        let filename = content_disposition
-            .get_filename()
-            .map_or_else(|| "unknown".to_string(), |name| name.to_string());
+        let filename = field
+            .content_disposition()
+            .and_then(|cd| cd.get_filename())
+            .map(|name| name.to_string())
+            .unwrap_or_else(|| "unknown".to_string());
         let filepath = format!("/data/uploads/{}", filename);
         let mut f = fs::File::create(&filepath)?;
         let mut field = field;
@@ -22,4 +23,4 @@ pub async fn save_file(mut payload: Multipart) -> Result<String, io::Error> {
         file_path = filepath;
     }
     Ok(file_path)
-}
+            }
