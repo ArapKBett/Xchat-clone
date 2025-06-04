d68199fa65d811db99c646ddccaa309874182bb8
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-04 07:36:07 -0400

    Update crypto.rs

--- src/crypto.rs
+++ src/crypto.rs
@@ -4,7 +4,7 @@ use aes_gcm::aead::Aead;
 use rand::Rng;
 use hex;
 
-#[derive(Clone)] // Added Clone derivation
+#[derive(Clone)]
 pub struct Crypto {
     secp: Secp256k1<secp256k1::All>,
 }
@@ -24,18 +24,20 @@ impl Crypto {
     pub fn encrypt_message(&self, message: &str, shared_secret: &[u8; 32]) -> (String, String) {
         let key = Key::<Aes256Gcm>::from_slice(shared_secret);
         let cipher = Aes256Gcm::new(key);
-        let nonce = Nonce::from_slice(&rand::thread_rng().gen::<[u8; 12]>());
+        let nonce_bytes = rand::thread_rng().gen::<[u8; 12]>();
+        let nonce = Nonce::from_slice(&nonce_bytes);
         let ciphertext = cipher.encrypt(nonce, message.as_bytes()).unwrap();
         (
             hex::encode(ciphertext),
-            hex::encode(nonce),
+            hex::encode(nonce_bytes),
         )
     }
 
     pub fn decrypt_message(&self, ciphertext: &str, nonce: &str, shared_secret: &[u8; 32]) -> String {
         let key = Key::<Aes256Gcm>::from_slice(shared_secret);
         let cipher = Aes256Gcm::new(key);
-        let nonce = Nonce::from_slice(&hex::decode(nonce).unwrap());
+        let nonce_bytes = hex::decode(nonce).unwrap();
+        let nonce = Nonce::from_slice(&nonce_bytes);
         let plaintext = cipher.decrypt(nonce, hex::decode(ciphertext).unwrap().as_ref()).unwrap();
         String::from_utf8(plaintext).unwrap()
     }
@@ -45,4 +47,4 @@ impl Crypto {
         let shared = [0u8; 32];
         shared
     }
-    }
+            }
