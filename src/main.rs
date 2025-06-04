bba336876228db3030fc6c41d4f2b147dd67a91e
Arap Bett <kipkorirjob@kabarak.ac.ke>
2025-06-04 05:45:50 -0400

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use actix_web::{App, HttpServer, middleware::Logger};
+use sqlx::SqlitePool;
+use xchat_clone::{db, crypto::Crypto, routes};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+    let pool = db::init_db().await;
+    let crypto = Crypto::new();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(crypto.clone()))
+            .wrap(Logger::default())
+            .route("/register", web::post().to(routes::register_user))
+            .route("/message", web::post().to(routes::send_message))
+            .route("/messages/{user_id}", web::get().to(routes::get_user_messages))
+            .route("/upload", web::post().to(routes::upload_file))
+            .route("/signal", web::post().to(routes::signal))
+            .route("/", web::get().to(routes::serve_index))
+            .service(actix_files::Files::new("/static", "./static").show_files_listing())
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+      }
